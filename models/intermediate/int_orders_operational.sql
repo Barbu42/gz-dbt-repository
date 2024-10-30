ccae2fce2e8187cfa37f390c00b91283704b89cf
jm.95360@gmail.com <jm.95360@gmail.com>
2024-10-30 10:47:32 +0000

    ops margin 2nd essai

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,10 +1,14 @@
-Select
-orders_id
-,date_date
-,quantity
-,revenue
-,(margin-ship_cost-shipping_fee-logcost) AS operational_margin
-FROM {{ref("int_orders_margin")}}
-JOIN {{ref("stg_raw__ship")}}
-USING (orders_id)
-ORDER BY orders_id DESC
+o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
